2fa992c0cd254e4e22752360be224b5729ddd047
siham1230 <ouchrifsiham86@gmail.com>
2025-11-27 11:50:45 +0100

    Implement authentication and team management features with routes and controllers

--- src/app.js
+++ src/app.js
@@ -5,9 +5,12 @@ require("dotenv").config();
 
 const app = express();
 
+const authRoutes = require("./routes/authRoutes");
+
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Test route
 app.get("/", (req, res) => {
@@ -25,9 +28,22 @@ sequelize
   .then(() => console.log("📦 Modèles synchronisés avec PostgreSQL"))
   .catch((err) => console.log(err));
 
+// Routes
+app.use("/api/auth", authRoutes);
+
+const teamRoutes = require("./routes/teamRoutes");
+
+//app.use(express.json());
+
+app.use("/api", teamRoutes); // prefix
+
 // Start server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Serveur lancé sur le port ${PORT}`);
 });
+
+
+
+
